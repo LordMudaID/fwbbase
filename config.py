221895f3fb095696a361cd88ed18be7621ff4f6d
LordMuda ID <91729945+LordMudaID@users.noreply.github.com>
2023-05-20 20:03:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 api_id = int(os.environ.get("API_ID", "24465982"))
 api_hash = os.environ.get("API_HASH", "2b3131b7d3f6a42bd4ae1ba3b58c11c4")
-bot_token = os.environ.get("BOT_TOKEN", "5974054493:AAFTQvELK38jPc5p1JBRN5ZuUlTTbUmcFT4")
+bot_token = os.environ.get("BOT_TOKEN", "5976774379:AAGl_UVqa7Z7ctX11wBrekxtX1TH6QwZwb8")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://owl:<password>@fwb.ouw7bsq.mongodb.net/?retryWrites=true&w=majority")
